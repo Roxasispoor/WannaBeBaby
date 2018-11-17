b6f347c4ec99da37a46c79947496c4634f1a007c
Pierrick Pamart <pierrick.pamart@telecom-sudparis.eu>
2018-11-17 12:19:03 +0100

    beta of input manager

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public PlayerInput input;
+
 	// Use this for initialization
 	void Start () {
-		
+        Debug.Assert(input != null);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    [SerializeField]
+    private int controllerNumber = -1;
+
+    public float leftHorizontal;
+    public float leftVertical;
+    public float rightHorizontal;
+    public float rightVertical;
+    public bool bumpLeft;
+    public bool bumpRight;
+    public bool TriggerLeft;
+    public bool TriggerRight;
+
+
+    public bool ControllerConnected
+    {
+        get
+        {
+            return controllerNumber > 0;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void SetJoystickNumber(int num)
+    {
+        controllerNumber = num;
+    }
+}
