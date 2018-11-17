bc00cbd4765afdb78816deca091b75eaa5981ef1
ufeii <yufyoceane@gmail.com>
2018-11-17 17:06:03 +0100

    UI v1

---
+++ Assets/Scripts/UIManagerP1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIManagerP1 : MonoBehaviour {
+
+    public Slider slider;
+    public TMPro.TextMeshProUGUI textP;
+    public Character character;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        slider.value = character.Age;
+        textP.text = character.Age.ToString();
+    }
+}
