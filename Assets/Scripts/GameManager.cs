bd77afb926629088d5c2b519222afa482e7e9f05
Yann <26348667+Mrplobi@users.noreply.github.com>
2018-11-17 22:29:20 +0100

    Main Map et game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,10 @@ public class GameManager : MonoBehaviour {
     private static GameManager instance;
     public List<Character> characters;
     public List<Player> players;
+    public Canvas Canva;
+    public UIManager[] UIBatchs;
+    public GameObject characterPrefab;
+    public Timer timer;
 
     private void Awake()
     {
@@ -30,14 +34,23 @@ public class GameManager : MonoBehaviour {
 
     private void OnEnable()
     {
-        SceneManager.sceneLoaded += InitialyzeMainScene;
+        SceneManager.sceneLoaded += InitializeMainScene;
     }
 
-    public void InitialyzeMainScene(Scene scene, LoadSceneMode sceneMode)
+    public void InitializeMainScene(Scene scene, LoadSceneMode sceneMode)
     {
         if (scene.name.Contains("MainScene"))
-        { 
-            
+        {
+            Canva = GameObject.Find("UI").GetComponent<Canvas>();
+            timer = GameObject.Find("Timer").GetComponent<Timer>();
+            GameObject[] UIBatchs = GameObject.FindGameObjectsWithTag("UIBtach");
+            for (int i = 0; i< players.Count; i++)
+            {
+                Character character = Instantiate(characterPrefab, new Vector3(-7.5f + i * 3, -1.2f), Quaternion.identity).GetComponent<Character>();
+                character.player = players[i];
+                UIBatchs[i].gameObject.SetActive(true);
+                UIBatchs[i].GetComponent<UIManager>().character = character;
+            }
         }
     }
 }
