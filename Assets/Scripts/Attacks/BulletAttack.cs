b4575ee6e067eaac5d02fba6f42f2e8b46f31800
manzano alban <manzanoalban@gmail.com>
2018-11-18 01:29:13 +0100

    balle code v0.1

---
+++ Assets/Scripts/Attacks/BulletAttack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletAttack : Attack {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.tag == character.tag)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            if (collision.GetComponent<AudioSource>() != null && hittingSound != null)
+            {
+                collision.GetComponent<AudioSource>().PlayOneShot(hittingSound);
+            }
+            if (collision.gameObject.GetComponent<Character>())
+            {
+                collision.gameObject.GetComponent<Character>().TakeDamage(damage);
+                character.TakeDamage(-damage);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/Attacks/GatlingAttack.cs
+++ Assets/Scripts/Attacks/GatlingAttack.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GatlingAttack : MonoBehaviour {
+public class GatlingAttack : Attack {
 
     public float range;
     public float maxSpeed;
     public float firerate;
-    public float time;
+    public float lastTimeShoot;
+    public GameObject prefabBullet;
+   
     // Use this for initialization
     void Start () {
+        Init();
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        if(Time.fixedTime-lastTimeShoot<firerate)
+        {
+            GameObject newObj=  Instantiate(prefabBullet, transform.position, Quaternion.identity);
+            newObj.GetComponent<Rigidbody2D>().velocity= character.transform.forward * maxSpeed;
+            lastTimeShoot = Time.fixedTime;
+            newObj.GetComponent<Attack>().character = gameObject.GetComponent<Character>();
+          
+        }
+
+
+    }
 }
